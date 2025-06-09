44adf09a96f9ceaf502db5e169bec774ab1c4335
Adones <adonesshpep@gmail.com>
2025-06-09 22:08:41 +0300

    ntier

---
+++ DataAccess/TaskRepository.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic; 
+using System.Linq; 
+using NTierTodoApp.Models;
+namespace NTierTodoApp.DataAccess
+{
+    /// <summary> 
+    /// ةركاذلا يف ةمئاق مادختساب ماهملا ةرادلإ تانايبلا عدوتسم. 
+    /// </summary> 
+    public class TaskRepository
+    {
+        private List<TaskItem> tasks = new List<TaskItem>
+        {
+            new TaskItem { Id = 1, Title = "ىلوأ ةمهم", IsComplete = false },
+            new TaskItem { Id = 2, Title = "ةيناث ةمهم", IsComplete = false }
+        };
+
+        public List<TaskItem> GetAll() => tasks;
+        public void Add(TaskItem task)
+        {
+            tasks.Add(task);
+        }
+        public TaskItem GetById(int id)
+        {
+            return tasks.FirstOrDefault(t => t.Id == id);
+        }
+        public void Delete(int id)
+        {
+            var task = tasks.FirstOrDefault(t => t.Id == id);
+            if (task != null)
+            {
+                tasks.Remove(task);
+            }
+        } 
+    }
+}
